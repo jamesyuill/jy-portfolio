692301ff75d3547768bd61ea47197502f836ffbe
James Yuill <jamesyuill@jamess-mbp.lan>
2023-08-06 21:19:47 +0100

    updates contact section with social links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 export const Contact = ({ contactSection }) => {
   return (
     <div className="contact" ref={contactSection}>
@@ -15,6 +17,39 @@ export const Contact = ({ contactSection }) => {
           <label htmlFor="message">MESSAGE: </label>
           <textarea id="message" />
         </form>
+      </div>
+
+      <div className="socials">
+        <nav className="social-links">
+          <a
+            className="link"
+            href="https://www.linkedin.com/in/jamesyuill/"
+            target="_blank"
+          >
+            LinkedIn
+          </a>
+          <a
+            className="link"
+            href="https://github.com/jamesyuill"
+            target="_blank"
+          >
+            Github
+          </a>
+          <a
+            className="link"
+            href="http://www.instagram.com/reanimatedgif"
+            target="_blank"
+          >
+            Instagram
+          </a>
+          <a
+            className="link"
+            href="http://www.twitter.com/jamesyuill"
+            target="_blank"
+          >
+            Twitter
+          </a>
+        </nav>
       </div>
     </div>
   );
