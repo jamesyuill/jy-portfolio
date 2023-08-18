76a9cb29099e82419a1deec4e2932c5e155a6701
James Yuill <jamesyuill@jamess-mbp.lan>
2023-08-18 10:58:35 +0100

    tweaks loading page text and header text

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ export default function Loading() {
   return (
     <div className="loading-wrapper">
       <div className="loading-div">
-        <h2>LOADING...</h2>
+        {/* <h1>LOADING...</h1> */}
         <div className="loading-anime">
           <img src="https://i.giphy.com/media/feN0YJbVs0fwA/giphy.webp" />
         </div>
